d4e66822e0129f9c56ae1f4299a40d65dcc8ddd6
lefsampaio <leticia.cosmetic2011@gmail.com>
2019-12-13 10:23:41 -0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD--dinG-lJPZrDAOfVueFQjBtVDgtv-JU",
+    authDomain: "burger-queen-59dde.firebaseapp.com",
+    databaseURL: "https://burger-queen-59dde.firebaseio.com",
+    projectId: "burger-queen-59dde",
+    storageBucket: "burger-queen-59dde.appspot.com",
+    messagingSenderId: "355992975866",
+    appId: "1:355992975866:web:66655ebea5ddb640d80c43",
+    measurementId: "G-B1JCV5L152"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
